cd80c3074636679a90699e2ba9e7959be5a7069c
Karsten Pruehss <Karsten.Pruehss@gmail.com>
2018-03-16 08:25:44 +1100

    changed linting config

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
   res.render('home');
 });
+
+app.get('/friends', (req, res) => {
+  res.render('friends');
+});
 // Server port config
 app.listen(3000, () => {
   console.log('Server STARTED');
